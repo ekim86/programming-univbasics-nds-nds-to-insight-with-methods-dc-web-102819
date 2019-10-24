ba5b04e3002189028779a37eb1b15aa0d3093cff
eunice kim <oonis86@gmail.com>
2019-10-24 19:37:29 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -19,15 +19,7 @@ def gross_for_director(director_data)
   while index < director_data[:movies].length do
     total += director_data[:movies][index][:worldwide_gross]
     index += 1
-    
-grand_total += vm[row_index][column_index][inner_index][:price]
-      inner_index += 1
-    end
-    column_index += 1
   end
-  row_index += 1
-end
- 
 
   total
 end
